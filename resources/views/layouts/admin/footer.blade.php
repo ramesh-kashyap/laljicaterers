ed75a80809399e072c5f6e4efdf3a2b7ca2ad23a
SAHILDEWAN23 <sahildewan2323@gmail.com>
2024-07-18 13:03:50 +0530

    thk h

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -53,6 +53,5 @@
     <script src="{{asset('')}}admin/js/custom.min.js"></script>
 	<script src="{{asset('')}}admin/js/deznav-init.js"></script>
 	{{-- <script src="{{asset('')}}admin/js/demo.js"></script> --}}
-    <script src="{{asset('')}}admin/js/styleSwitcher.js"></script>
 </body>
 </html>
